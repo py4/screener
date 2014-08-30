3c519b13fe2c1b932aea5357089d817d1fcba1c2
Pooya <ibtkm2009@gmail.com>
2014-08-30 16:16:02 +0430

    users' registration and login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+	def persian_number(str)
+		str = str.to_s if str.class == Fixnum
+		str ? str.unpack('U*').map{ |e| (0..9).to_a.include?(e-48) ? e + 1728 : e }.pack('U*') : ''
+	end
+
+	def persian_date(date)
+		(date ? persian_number(JalaliDate.new(date).format("%A %d %b %Y - %H:%m")) : '').split('-')[0]
+	end
+
+	def flash_class(level)
+		case level
+		when :notice then "alert alert-info"
+		when :success then "alert alert-success"
+		when :error then "alert alert-error"
+		when :alert then "alert alert-error"
+		end
+	end
+
+
 end
