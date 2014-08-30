73160b53e18b03f85e7900dda2187bcd221874c7
Pooya <ibtkm2009@gmail.com>
2014-08-30 11:40:49 +0430

    initial core of the project

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+gem 'thin'
+gem 'devise'
+gem 'simple_form'
+gem 'pg'
+gem 'gravatarify', '~> 3.0.0'
+gem 'jalalidate'
+
+group :development do
+	gem 'quiet_assets', :group => :development
+	gem 'pry-rails'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Screener::Application.config.secret_key_base = 'da2711d29411f298f2b1b8f81d8db230dac5670f2b576d5981c8ccd48fd7fa9189940ffa86bb869df5bb43a62f7e8160ab18ea80dea6cfdc8052748b3353cf39'
